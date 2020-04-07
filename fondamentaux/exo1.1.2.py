cec631b80353ce7abe77a462933af6b2a33bce88
mmokhlis <m.mokhlis.mohamed@gmail.com>
2020-04-07 19:31:34 +0200

    Resolve Exercice 1.1.2

--- fondamentaux/exo1.1.2.py
+++ fondamentaux/exo1.1.2.py
@@ -1,2 +1,13 @@
 import random
 
+
+userRand = int(input("Recherche par l'ordi \n"))
+
+rand = random.randint(0, 100)
+
+
+while rand != userRand:
+    print("Ma proposition est ", rand)
+    rand = random.randint(0, 100)
+
+print("La réponse était ", rand)
