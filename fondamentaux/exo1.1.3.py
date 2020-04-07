f7e7ab605f030ce61f68685ec8850fc7485cffd3
mmokhlis <m.mokhlis.mohamed@gmail.com>
2020-04-07 23:46:42 +0200

    Add exo 1.1.3 and edit Readme file

---
+++ fondamentaux/exo1.1.3.py
@@ -0,0 +1,8 @@
+
+
+def estPalindrome(string):
+    string = string.replace(" ", "")
+    for i in range(0, len(string) // 2):
+        if string[i] != string[len(string) - i - 1]:
+         return "False"
+    return "True"
