217adfec116b4266133e2aea5fcd477cef3ae29c
mmokhlis <m.mokhlis.mohamed@gmail.com>
2020-04-08 10:11:03 +0200

    refactor exo1.1.1

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ L'ensemble des excercices de la formation
 Exercice 1.1.1
 Construire un jeu qui pour tire aléatoirement un nombre entre 1 et 100 et demande à l'utilisateur de le deviner. Pour chaque entrée de l'utilisateur le programme affiche si la bonne réponse est supérieure ou inférieure. Il affiche aussi si la réponse est très proche (<=5), proche ( 6 à 10) ou supérieur.
 
-Exercice 1.1.2
+Exercice 1.1.2 
 Écrire un programme qui étant donné un nombre saisi par l'utilisateur entre 1 et 100 recherche ce nombre de façon efficace.
 
-Exercice 1.1.3
+Exercice 1.1.3 : 
 Réaliser un vérificateur de palindrome
 
 Exercice 1.1.4
--- fondamentaux/exo1.1.1.py
+++ fondamentaux/exo1.1.1.py
@@ -7,20 +7,17 @@ userRand = int(input("Devinez un nombre entre 1 et 100 \n"))
 
 def cheekranduser(usernmb, truerand):
 
-    def cheekdistance(a, b):
-        if abs(a-b) <= 5:
-            return "tres proche \n"
-        elif abs(usernmb - truerand in range(6, 11)):
-            return "proche \n"
-        elif a-b < 0:
-            return "c'est bcp plus \n"
-        else:
-            return "c'est bcp moins \n"
-
-    if usernmb == truerand:
+    if abs(usernmb-truerand) == 0:
         return "Exact!"
+    elif abs(usernmb-truerand) <= 5:
+        return "tres proche \n"
+    elif abs(usernmb - truerand in range(6, 11)):
+        return "proche \n"
+    elif usernmb-truerand < 0:
+        return "c'est bcp plus \n"
     else:
-        return cheekdistance(usernmb, truerand)
+        return "c'est bcp moins \n"
+
 
 cheek = cheekranduser(userRand, rand)
 
