f8c67e973d0725c0f8521e87d2ca0f7e213a9b72
mmokhlis <m.mokhlis.mohamed@gmail.com>
2020-04-16 18:00:52 +0200

    add cesar crypto function

---
+++ fondamentaux/node/cryptostring.js
@@ -0,0 +1,8 @@
+var crypto = require('crypto');
+function genererCle(message) {
+    let cle;
+    return cle;
+}
+
+function 
+crypto.randomBytes(64).toString('hex');
---
+++ fondamentaux/node/exo1.3.1.js
@@ -0,0 +1,23 @@
+function chiffreCesar(str, decalage) {
+let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let chiffre;
+let cesar ="";
+console.log(str);
+let k = decalage;
+
+if (decalage<0) decalage+=26;
+if (decalage>26) decalage-=26;
+for (let i = 0; i< str.length;i++){
+    if(str[i]==" ") {cesar+= str[i];}
+    else {
+        indexCesar= (alphabet.indexOf(str[i])+ k)%26;
+        cesar+= alphabet.charAt(indexCesar)
+        
+    }
+}
+return cesar;
+}
+
+console.log(chiffreCesar("ABCD",3));
+console.log(chiffreCesar("XYZ",3));
+
