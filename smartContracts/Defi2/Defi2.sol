12a9910b74e422ff980ddb93cba79e9051af8111
mmokhlis <m.mokhlis.mohamed@gmail.com>
2020-05-20 17:12:24 +0200

    reroganise code

--- smartContracts/Defi2/Defi2.sol
+++ smartContracts/Defi2/Defi2.sol
@@ -9,32 +9,53 @@
         
         //Mécanisme de réputation
         uint private indexDemande;
-        struct Illustrateur {
+        
+        enum EtatDemande { OUVERTE, ENCOURS, FERMEE }
+        
+         struct Illustrateur {
             address adresseIllustrateur;
             uint reputation; //Pour représenter la réputation, nous allons associer chaque utilisateur à une valeur entière
             string nom ;//Un nom est aussi associé à l’adresse
             bool estMembre; // si il est membre ou pas
-        }
-        mapping (address => Illustrateur) illustrateurs;
-        address[] public illustrateursAccts;
-        
-        modifier illustrateurInscrit{
-            require(illustrateurs[msg.sender].estMembre, "inscrivez vous");
-            _;
         }
         
         struct Demandeur {
             address adresseDemandeur;
             bool estInscrit;
         }
+        
+       
+        struct Demande {
+          Demandeur demandeur;
+          uint id; // id de la demande
+          uint remuneration ;       // (en wei)
+          uint delai ;              //seconds
+          string description;       //(champ texte)
+          uint reputationMinumum;   //Définir une réputation minimum pour pouvoir postuler
+          Illustrateur candidatChoisi;
+          bytes32 hashLienLivraison;
+          mapping (address => Illustrateur) candidats;
+          EtatDemande etatDemande  ;//Définir une réputation minimum pour pouvoir postuler
+        }
+        
+        mapping (address => Illustrateur) illustrateurs;
+        address[] public illustrateursAccts;
         mapping (address => Demandeur) demandeurs;
-        address[] public demandeursAccts;
+        Demande [] demandes;
+        
+        
+        
+        modifier illustrateurInscrit{
+            require(illustrateurs[msg.sender].estMembre, "inscrivez vous");
+            _;
+        }
+        
         modifier demandeurInscrit{
             require(demandeurs[msg.sender].estInscrit, "inscrivez vous");
             _;
         }
         //Lorsqu’un nouveau participant rejoint la plateforme, il appelle la fonction inscription() qui lui donne une réputation de 1. Il
-        function inscription(string memory nom) public {
+        function inscription(string calldata nom) external {
             // on verifie si il est membre ou pas 
             require(!(illustrateurs[msg.sender].estMembre), "vous êtes déja inscrit");
             illustrateurs[msg.sender].adresseIllustrateur = msg.sender;
@@ -49,22 +70,6 @@
             demandeurs[msg.sender].estInscrit = true;
         }
         
-        enum EtatDemande { OUVERTE, ENCOURS, FERMEE } 
-        
-        struct Demande {
-          Demandeur demandeur;
-          uint id; // id de la demande
-          uint remuneration ;       // (en wei)
-          uint delai ;              //seconds
-          string description;       //(champ texte)
-          uint reputationMinumum;   //Définir une réputation minimum pour pouvoir postuler
-          Illustrateur candidatChoisi;
-          bytes32 hashLienLivraison;
-          mapping (address => Illustrateur) candidats;
-          EtatDemande etatDemande  ;//Définir une réputation minimum pour pouvoir postuler
-        }
-        
-        Demande [] demandes;
         
         function ajouterDemande(uint _remuneration, uint _delai, string memory _description, uint _reputationMinumum) public payable demandeurInscrit{
             require(msg.value >= _remuneration + (_remuneration * 2/100));
@@ -82,7 +87,7 @@
         ///----------------Mécanisme de contractualisation
     //Créer une fonction postuler() qui permet à un indépendant de proposer ses services. 
     //Il est alors ajouté à la liste des candidats.
-    function postuler(uint idDemande) public illustrateurInscrit {
+    function postuler(uint idDemande) external illustrateurInscrit {
         require(illustrateurs[msg.sender].reputation >= demandes[idDemande-1].reputationMinumum, 
         "vous n'avez pas la réputation qu'il faut");
         Illustrateur memory candidat;
@@ -94,7 +99,7 @@
     //La demande est alors ENCOURS jusqu’à sa remise
     //enum EtatDemande { OUVERTE, ENCOURS, FERMEE } 
     //EtatDemande etatDemande  ;
-    function accepterOffre(uint idDemande, address addressCandidat ) public demandeurInscrit {
+    function accepterOffre(uint idDemande, address addressCandidat ) external demandeurInscrit {
         require(demandes[idDemande-1].demandeur.adresseDemandeur == msg.sender,
         "vous n etes pas owner de cette demande");
         require(illustrateurs[addressCandidat].adresseIllustrateur == demandes[idDemande-1].candidats[addressCandidat].adresseIllustrateur,
@@ -106,7 +111,7 @@
     //Ecrire une fonction livraison() qui permet à l’illustrateur de remettre le hash du lien où se trouve son travail. 
     //Les fonds sont alors automatiquement débloqués et peuvent être retirés par l’illustrateur. 
     //L’illustrateur gagne aussi un point de réputation
-    function livraison(uint idDemande,string memory lienLivraison) public illustrateurInscrit {
+    function livraison(uint idDemande,string memory lienLivraison) external illustrateurInscrit {
         require(illustrateurs[msg.sender].adresseIllustrateur == demandes[idDemande-1].candidatChoisi.adresseIllustrateur,
         "vous n'etes pas le candidat choisi pour cette demande");
         bytes32 hashLienLivraison = keccak256(bytes(lienLivraison));
