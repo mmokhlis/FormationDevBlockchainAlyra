848c68e982754d7e5208d7e82f6fd500d36c4c14
mmokhlis <m.mokhlis.mohamed@gmail.com>
2020-05-22 04:52:43 +0200

    la partie react

--- smartContracts/DEFI2-FINAL/client/src/App.js
+++ smartContracts/DEFI2-FINAL/client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import MarketPlace from "./contracts/MarketPlace.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { indexDemande, id,remuneration,delai,reputationMinumum,etatDemande, web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
     try {
@@ -17,15 +17,15 @@ class App extends Component {
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const deployedNetwork = MarketPlaceContract.networks[networkId];
       const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
+        MarketPlaceContract.abi,
+        deployedNetwork.address,
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, contract: instance });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -34,19 +34,23 @@ class App extends Component {
       console.error(error);
     }
   };
+loadData = async () => {
+  const {contract } = this.state;
+  let indexDemande =await contract.indexDemande;
+  this.setState({indexDemande});
+}
+  // runExample = async () => {
+  //   const { accounts, contract } = this.state;
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+  //   // Stores a given value, 5 by default.
+  //   await contract.methods.set(5).send({ from: accounts[0] });
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+  //   // Get the value from the contract to prove it worked.
+  //   const response = await contract.methods.get().call();
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
+  //   // Update state with the result.
+  //   this.setState({ storageValue: response });
+  // };
 
   render() {
     if (!this.state.web3) {
@@ -54,17 +58,8 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>Bonjour!</h1>
+        <div>le nombre des demandes est : {this.state.indexDemande}</div>
       </div>
     );
   }
--- smartContracts/DEFI2-FINAL/contracts/MarketPlace.sol
+++ smartContracts/DEFI2-FINAL/contracts/MarketPlace.sol
@@ -1,4 +1,4 @@
-    pragma solidity ^0.6.3;
+    pragma solidity >=0.5.16 <=0.6.3;
 
     //pragma experimental ABIEncoderV2;
 
